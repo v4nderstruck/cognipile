a24ff34db1bfb067ccd2c964e564cc1bf001ea62
zensayyy <zensayyy@gmail.com>
2023-02-07 16:32:37 +0100

    apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
+import apolloClient from '@/lib/apollo'
 import '@/styles/globals.css'
+import { ApolloProvider } from '@apollo/client'
 import { MantineProvider } from '@mantine/core'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider withNormalizeCSS withGlobalStyles>
-      <Component {...pageProps} />
-    </MantineProvider>
+    <ApolloProvider client={apolloClient}>
+      <MantineProvider withNormalizeCSS withGlobalStyles>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </ApolloProvider>
   )
 }
